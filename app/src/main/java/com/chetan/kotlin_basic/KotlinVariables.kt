6f1cba828ce5d7c090baad0742063e72c1c217e2
cbbchetan <cbb.chetan@gmail.com>
2021-04-06 20:51:36 +0530

    Kotlin first program and also variable example
    First Hello world and  variable program of kotlin android language.

---
+++ app/src/main/java/com/chetan/kotlin_basic/KotlinVariables.kt
@@ -0,0 +1,9 @@
+package com.chetan.kotlin_basic
+
+fun main(){
+    var myVar = "Chetan"
+    val myVal = "Pavan"
+    myVar = "Nandu"
+    println("Hello World "+myVar)
+    print("Hello World "+myVal)
+}
